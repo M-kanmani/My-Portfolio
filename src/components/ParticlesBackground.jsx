58ee0b4c6311a26165152fb27fe3e69fa1052779
M-kanmani <kanmanimurugesan1526@gmail.com>
2026-08-26 21:22:55 +0530

    Update to new tsparticles library for Vercel deployment

--- src/components/ParticlesBackground.jsx
+++ src/components/ParticlesBackground.jsx
@@ -1,57 +1,59 @@
-import Particles from "react-tsparticles";
-import { loadFull } from "tsparticles";
+import React, { useEffect, useState } from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { loadSlim } from "@tsparticles/slim";
 
 function ParticlesBackground() {
+  const [init, setInit] = useState(false);
 
-  const particlesInit = async (engine) => {
-    await loadFull(engine);
-  };
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      await loadSlim(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+
+  if (!init) {
+    return null;
+  }
 
   return (
     <Particles
-      init={particlesInit}
+      id="tsparticles"
       options={{
         background: {
           color: {
             value: "#050816",
           },
         },
-
         fpsLimit: 60,
-
         particles: {
           number: {
             value: 60,
           },
-
           color: {
             value: "#3B82F6",
           },
-
           links: {
             enable: true,
             color: "#3B82F6",
             distance: 150,
             opacity: 0.3,
           },
-
           move: {
             enable: true,
             speed: 1,
           },
-
           size: {
             value: {
               min: 1,
               max: 3,
             },
           },
-
           opacity: {
             value: 0.5,
           },
         },
-
         interactivity: {
           events: {
             onHover: {
@@ -60,7 +62,6 @@ function ParticlesBackground() {
             },
           },
         },
-
         detectRetina: true,
       }}
       className="absolute inset-0"
